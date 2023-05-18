041b60b8820a225f595bf087d0b66f412e938b99
윤지현 <gus991121@naver.com>
2023-05-18 10:54:13 +0900

    build accounts model

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -1,6 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+# from django.contrib.auth.hashers import make_password
 
 # Create your models here.
-class User(AbstractUser):
-    pass
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('이메일을 입력해주세요')
+        
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=10)
+    nickname = models.CharField(max_length=10, unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    user_image = models.ImageField(upload_to='media/profile/', null=True, blank=True)
+    favorite_movie = models.CharField(max_length=255, null=True, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS=['username', 'nickname']
