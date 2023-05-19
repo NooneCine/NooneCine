10b35fff8483389fd624b4b7d1c22961bd482d5f
조원주 <dnjswn9178@hanmail.net>
2023-05-19 09:01:06 +0900

    design(Header):nav bar css change

--- final-pjt-front/src/components/Header.vue
+++ final-pjt-front/src/components/Header.vue
@@ -1,18 +1,21 @@
 <template>
-  <nav class="navbar">
-    <div class="container">
-      <router-link to="/" class="nav-item">
+  <div>
+    <div class="navbar top">
+      <router-link to="/">
         <img src="@/assets/default.png" alt="Bootstrap" height="48">
       </router-link>
-      <router-link to="/" class="nav-item">HOME</router-link> 
-      <router-link to="/movie" class="nav-item">MOVIE</router-link> 
-      <router-link to="/community" class="nav-item">COMMUNITY</router-link> 
-      <router-link to="/my-page" class="nav-item">MY PAGE</router-link>
+      <div class="mt-0 top nav-a">
+        <router-link to="/" class="m-3">HOME</router-link> 
+        <router-link to="/movie" class="m-3">MOVIE</router-link> 
+        <router-link to="/community" class="m-3">COMMUNITY</router-link> 
+        <router-link to="/my-page" class="m-3">MY PAGE</router-link>
+      </div>
+      <div>
+        <router-link to="/signup" class="me-3 text-primary">SIGN UP</router-link>
+        <router-link to="/login" class="btn btn-primary text-white">LOGIN</router-link>
+      </div>
     </div>
-
-    <router-link to="/signup">SIGN UP</router-link> |
-    <router-link to="/login">LOGIN</router-link>
-  </nav>
+  </div>
 </template>
 
 <script>
@@ -25,17 +28,22 @@ export default {
 </script>
 
 <style>
-nav {
-  padding: 30px;
+.top {
+  padding: 10px 20px;
+  margin-right: 20px;
 }
 
-nav a {
+.top a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #FFC0B4;
   text-decoration-line: none;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.top a.router-link-exact-active {
+  color: #FF2700;
+}
+
+.nav-a {
+  font-size: 20px;
 }
 </style>
