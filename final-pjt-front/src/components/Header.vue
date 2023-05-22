e2d507a7d4ea436863b3bed65ec1a4a4268d077c
wonju <dnjswn9178@hanmail.net>
2023-05-22 12:01:12 +0900

    design(login): layout

--- final-pjt-front/src/components/Header.vue
+++ final-pjt-front/src/components/Header.vue
@@ -10,7 +10,7 @@
         <router-link to="/community" class="m-3">COMMUNITY</router-link> 
         <router-link to="/my-page" class="m-3">MY PAGE</router-link>
       </div>
-      <div>
+      <div class="d-flex">
         <router-link to="/search">
           <svg @click="searchBar" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="secondary" class="bi bi-search me-4" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
