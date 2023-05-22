191e8650b1a223f211c0242668d0de0a40892a2a
wonju <dnjswn9178@hanmail.net>
2023-05-22 12:59:23 +0900

    file(migrate): migrate file delete

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -86,7 +86,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return {user, token}
+        return user
         
 # 로그인
 class LoginSerializer(serializers.Serializer):
