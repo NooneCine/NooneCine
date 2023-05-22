84c84d1bb29bf57cb232b69ef449b0c811f857ae
wonju <dnjswn9178@hanmail.net>
2023-05-22 09:36:49 +0900

    feat(community): community add

---
+++ final-pjt-front/src/components/CommunityList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Community</h3>
+    <CommunityListItem v-for="post in posts" :key="post.id" :post="post"/>
+  </div>
+</template>
+
+<script>
+import CommunityListItem from '@/components/CommunityListItem.vue'
+
+export default {
+  name: 'CommunityList',
+  components: {
+    CommunityListItem,
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
