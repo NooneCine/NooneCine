2ffb2c025ec707580599ed3e4e5efa882f44be0a
윤지현 <gus991121@naver.com>
2023-05-23 11:55:28 +0900

    feat(post): add like

--- final-pjt-back/community/serializers.py
+++ final-pjt-back/community/serializers.py
@@ -18,8 +18,9 @@ class CommentSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     comment_set = CommentSerializer(many=True, read_only=True)
     comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
-    # user = serializers.CharField(source='user', read_only=True)
     image = serializers.ImageField(use_url=True, required=False)
+    # 해당 포스터에 요청한 사용자가 좋아요를 눌렀는지 여부를 나타냄
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
@@ -29,6 +30,12 @@ class PostSerializer(serializers.ModelSerializer):
             'title': {'required': True},
             'content': {'required': True},
         }
+    
+    def get_is_liked(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return obj.is_liked_by(user)
+        return False
 
 
 
